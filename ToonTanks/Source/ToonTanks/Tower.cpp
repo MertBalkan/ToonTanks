b1a58b0d7728e07916225734898cb36ecfa45d65
TheMOON <mert__balkan@hotmail.com>
2022-03-19 15:51:42 +0300

    Timers

--- ToonTanks/Source/ToonTanks/Tower.cpp
+++ ToonTanks/Source/ToonTanks/Tower.cpp
@@ -6,22 +6,43 @@ void ATower::BeginPlay()
 {
 	Super::BeginPlay();
 	Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
-}
 
+	GetWorldTimerManager().SetTimer
+	(
+		FireRateTimerHandle,
+		this,
+		&ATower::CheckFireCondition,
+		FireRate,
+		true
+	);
+}
 void ATower::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	
+	if(InFireRange())
+	{
+		// If in range, rotate turret toward Tank
+		RotateTurret(Tank->GetActorLocation());
+	}
+}
+
+void ATower::CheckFireCondition()
+{
+	if(InFireRange())
+	{
+		Fire();
+	}
+}
+
+bool ATower::InFireRange() const
+{
+	float Distance = 0;
+	
 	// Find the distance to the tank, if it's not null
 	if(Tank)
 	{
-		float Distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
-		// Check to see if the Tank is in range
-
-		if(Distance <= FiringRange)
-		{
-			// If in range, rotate turret toward Tank
-			RotateTurret(Tank->GetActorLocation());
-		}
+		Distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
 	}
+	return Distance <= FiringRange;
 }
--- ToonTanks/Source/ToonTanks/Tower.h
+++ ToonTanks/Source/ToonTanks/Tower.h
@@ -15,7 +15,12 @@ protected:
 	virtual void BeginPlay() override;
 private:
 	class ATank* Tank;
+	FTimerHandle FireRateTimerHandle;
 
 	UPROPERTY(Category="Combat", EditAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess="true"))
 	float FiringRange = 300.0f;
+	float FireRate = 2.0f; 
+	
+	void CheckFireCondition();
+	bool InFireRange() const;
 };
